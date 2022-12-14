636c67845933c031b72a6bb403c8b109771f833e
Iker Trueba Escobal <itrueba-@c3r3s4.42urduliz.com>
2022-12-14 11:48:44 +0100

    Last Version

--- Makefile
+++ Makefile
@@ -1,28 +1,28 @@
 
 NAME		= libftprintf.a
 INCLUDE		= include
+LIBFTA		= libft/libft.a
 LIBFT		= libft
 CC			= gcc
 CFLAGS		= -Wall -Werror -Wextra
-RM			= rm -f
+RM			= rm -rf
 AR			= ar rcs
 
 #Sources
 
-SRCS	=	ft_hex.c ft_printf.c ft_str_nbr.c ft_uitoa.c 
+SRCS	=	ft_hex.c ft_printf.c ft_str_nbr.c ft_uitoa.c
 
 OBJS	= 	$(SRCS:.c=.o)
 
 all:		$(NAME)
 
-$(NAME):	$(OBJS)
+$(NAME):	$(OBJS) 
 			@make -C $(LIBFT)
-			@mv libft/libft.a .
-			@$(AR) $(NAME) $(OBJS)
+			@$(AR) $(NAME) $(LIBFTA) $(OBJS)
 
 clean:
 			@make -C $(LIBFT) clean
-			@$(RM) -rf $(OBJS)
+			@$(RM) $(OBJS)
 
 fclean:		clean
 			@make -C $(LIBFT) fclean
--- ft_printf.h
+++ ft_printf.h
@@ -6,19 +6,18 @@
 /*   By: itrueba- <itrueba-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 11:08:55 by itrueba-          #+#    #+#             */
-/*   Updated: 2022/12/13 19:38:54 by itrueba-         ###   ########.fr       */
+/*   Updated: 2022/12/14 11:16:53 by itrueba-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
+# include "libft/libft.h"
 # include <stdarg.h>
 # include <stdio.h>
-# include "libft/libft.h"
 
-int	ft_printf(const char *str, ...);
-int	ft_convert(va_list args, const char format);
+int		ft_printf(const char *str, ...);
 char	*ft_uitoa(unsigned int nb);
 int		p_ptr(long nbr);
 int		p_hex(unsigned int nbr, char format);
